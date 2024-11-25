77dd733994d4061b596ef8a19e18032997891172
Teo <teo.kohan@gmail.com>
2024-11-25 19:17:28 -0300

    Day 1

---
+++ day_1/calibration.py
@@ -0,0 +1,27 @@
+from itertools import cycle, tee
+from collections.abc import Iterable
+import re
+
+with open('input') as input:
+    raw_readings = input.read()
+
+readings: Iterable[int] = map(int, re.findall(r'([+-]\d+)', raw_readings))
+
+def final_reading(readings: Iterable[int]) -> int:
+    return sum(readings)
+
+def repeat_reading(readings: Iterable[int]) -> int:
+    current_value: int = 0
+    read_values: set[int] = set()
+    loop: Iterable[int] = cycle(readings)
+
+    while not current_value in read_values:
+        read_values.add(current_value)
+        current_value += next(loop)
+    
+    return current_value
+
+with open('output', 'w') as output:
+    readings_final, readings_repeat = tee(readings)
+    output.write( str(final_reading(readings_final)) + '\n')
+    output.write( str(repeat_reading(readings_repeat)) + '\n')
